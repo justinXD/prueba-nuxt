0eb1c5d5c6a2bd9825e5f1ecc419639d8d2d7c5b
Justin D <isaac_justin@hotmail.com>
2022-10-27 16:04:55 -0500

    feat(proyect): cambios varios, pagina en dark mode por default

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'green-nuxt': '#00dc82',
+        'purple-p': '#A916DB',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
